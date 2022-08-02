ac6ff9eb1c02c5cf615ab39aec4e81f19a78f1ad
LAPTOT_CARLA\carlu <carla.diaz@xarxatecactiva.com>
2022-08-02 11:36:23 +0200

    Eliminar comentarios

--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,3 +1,20 @@
+function añadirFilas() {
+    let table = document.getElementById("probandoTabla");
+    let row = table.insertRow(0);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    cell1.innerHTML = "1";
+    cell2.innerHTML = "2";
+    cell3.innerHTML = "3";
+    cell4.innerHTML = "4";
+}
+
+
+
+
+
 /*function agregarFila(){
     let tabla = document.getElementById("probandoTabla");
     let tbody = document.createElement("tbody");
@@ -25,15 +42,3 @@ for(i=0;i<=4; i++){
 
 
 
-function añadirFilas() {
-    let table = document.getElementById("probandoTabla");
-    let row = table.insertRow(0);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    cell1.innerHTML = "1";
-    cell2.innerHTML = "2";
-    cell3.innerHTML = "3";
-    cell4.innerHTML = "4";
-  }
